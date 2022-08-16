7d7d9209de068e8ed4d777101032596acaef860a
LimHun <dlagnskkld3@gmail.com>
2022-08-16 16:57:41 +0900

    first commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by tunko on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
